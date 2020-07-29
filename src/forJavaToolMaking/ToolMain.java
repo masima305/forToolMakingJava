2f74276b03c2ceac73a6556b9fe87064468cc5ac
masima305 <masima305@gmail.com>
2020-07-29 23:07:30 +0900

    네이버 크롤링 선택자 찾는방법 알아냄

--- src/forJavaToolMaking/ToolMain.java
+++ src/forJavaToolMaking/ToolMain.java
@@ -10,24 +10,38 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class ToolMain {
-	public static void main(String[] args) {
+	
+	static String ARTICLE_URL = "https://finance.naver.com/item/main.nhn?code=035720";
+	
+	public static void main(String[] args) throws IOException {
 		System.out.println("helloWorld!");
+
+		Document rawData = Jsoup.connect(ARTICLE_URL)
+                .timeout(5000)
+                .get();
+		
+		
 		Document doc = null;
 		
-		try {
-			doc = Jsoup.connect("https://finance.naver.com/item/main.nhn?code=035720").get();
-		} catch (IOException e) {	
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//Document google3 = response.parse();
+//		
+//		
+		String elen = rawData.html();
+		System.out.println(elen);
+		// 당기 순이익 가져올 수 있도록 확인해야함.(selector 확인)
+		// selector는 구글 개발자도구 > 오른쪽버튼 > copySelector 하면 바로 딸수 있음 개이득!!!
+		Elements element = rawData.select("#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(6) > td:nth-child(2)");
+		System.out.println(element.text());
+		element = rawData.select("#content > div.section.cop_analysis > div.sub_section > table > tbody > tr:nth-child(6) > td.null.last.cell_strong");
+		System.out.println(element.text().equals("")); //true
+
+		//array로 확인하는게 맞는건지??
+		//ArrayList<String> a  = (ArrayList<String>) element.eachText();
 		
-		Elements element = doc.select("div.h_th2 th_cop_anal10");
-		
-		ArrayList<String> a  = (ArrayList<String>) element.eachText();
-		
-		for (String b : a) {
-			System.out.println(b);
-		}
+		//for.... 
+//		for (String b : a) {
+//			System.out.println(b);
+//		}
 		
 		
 	}
